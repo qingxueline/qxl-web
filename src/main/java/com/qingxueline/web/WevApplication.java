bf9666c18af58acd5e483a5daeabde190a318906
lyl <251409016@qq.com>
2019-08-01 10:11:48 +0800

    qxl-web

---
+++ src/main/java/com/qingxueline/web/WevApplication.java
@@ -0,0 +1,14 @@
+package com.qingxueline.web;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author lyl
+ */
+@SpringBootApplication
+public class WevApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WevApplication.class, args);
+    }
+}
---
+++ src/main/java/com/qingxueline/web/biz/UserController.java
@@ -0,0 +1,15 @@
+package com.qingxueline.web.biz;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * @author lyl
+ */
+@RestController
+@RequestMapping(value = "/api/user")
+public class UserController {
+    @RequestMapping("/say")
+    public String say() {
+        return "Say Hello Docker";
+    }
+}
