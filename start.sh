2d6105c0dcbfb00a80f0a79d42618a6e440b27d5
lyl <251409016@qq.com>
2019-08-02 10:35:47 +0800

    去除docker插件

--- start.sh
+++ start.sh
@@ -5,4 +5,3 @@ pwd
 #nohup java -javaXms128m -Xmx256m -jar -Dfile.encoding=utf-8 ${JAR_NAME} &
 nohup java -jar ${JAR_NAME}.jar>consoleMsg.log 2>&1 &
 pid='ps -ef | grep '${JAR_NAME}'.jar | grep -v grep'
-echo ${pid}
