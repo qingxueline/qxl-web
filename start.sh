d3513af95274dfcd814af1f6f20fdda9cd3c9517
lyl <251409016@qq.com>
2019-08-02 15:07:49 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 export BUILD_ID=dontKillMe
-JAR_NAME='qxl-web.jar'
-JAR_FILE=~/.jenkins/workspace/qxl-web/target/$JAR_NAME
-if [ ! -f "$JAR_FILE" ]; then
+JAR_NAME="qxl-web.jar"
+JAR_FILE=~/.jenkins/workspace/qxl-web/target/${JAR_NAME}
+execute_target=~/qxl-web/
+if [ ! -f ${JAR_FILE} ]; then
+  echo "qxl-web.jar not exist!"
   exit
 else
-  cp $JAR_FILE ~/qxl-web/$JAR_NAME
+  if [ ! -d ${execute_target} ]; then
+    echo "create directory"
+    mkdir -p ${execute_target}
+  fi
+  cp ${JAR_FILE} ~/qxl-web/${JAR_NAME}
 fi
 
 cd ~/qxl-web/ || exit
 pwd
 
 echo "Startup SpringBoot Application"
-#nohup java -javaXms128m -Xmx256m -jar -Dfile.encoding=utf-8 ${JAR_NAME} &
 nohup java -jar ${JAR_NAME} >consoleMsg.log 2>&1 &
 #pid='ps -ef | grep '${JAR_NAME}' |  grep -v grep | awk '{print $2}''
 #if [ -n "$pid" ]
