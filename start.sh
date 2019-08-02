6629fcfb250b1b50bdfc3e27a344306b4efc57c6
lyl <251409016@qq.com>
2019-08-02 15:35:53 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-# 必须添加此参数，否则Jenkins在执行完一个job就会把tamcat程序杀掉，导致执行[nohup java -jar ${JAR_NAME} >consoleMsg.log 2>&1 &]起不来
+
 export BUILD_ID=dontKillMe
-# 常量定义
+
 JAR_NAME="qxl-web.jar"
 JAR_FILE=~/.jenkins/workspace/qxl-web/target/${JAR_NAME}
 execute_target=~/qxl-web/
 
-# 判断是否已有启动的程序
-pid='ps -ef | grep '${JAR_NAME}' |  grep -v grep | awk '{print $2}''
-echo $pid
-if [ -n $pid ]
-then
-   kill -9 $pid
-fi
 
-# 对文件和文件路径进行判断
+
 if [ ! -f ${JAR_FILE} ]; then
   echo "qxl-web.jar not exist!"
   exit
@@ -26,8 +19,13 @@ else
   cp ${JAR_FILE} ~/qxl-web/${JAR_NAME}
 fi
 
-# 执行新程序
 cd ~/qxl-web/ || exit
 echo "Go to the execution directory >> $(pwd)"
 echo "Startup SpringBoot Application"
 nohup java -jar ${JAR_NAME} >consoleMsg.log 2>&1 &
+
+pid='ps -ef | grep '${JAR_NAME}' |  grep -v grep | awk '{print $2}''
+echo $pid
+if [ -n $pid ]; then
+    kill -9 $pid
+fi
