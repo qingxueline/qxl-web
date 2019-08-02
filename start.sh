9aa150a632c212bd0dc566888dd7ba5ac4cd4786
lyl <251409016@qq.com>
2019-08-02 09:51:38 +0800

    去除docker插件

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 JAR_NAME='qxl-web.jar'
 cd ~/.jenkins/workspace/qxl-web/target
 pwd
-nohup java -javaXms128m -Xmx256m -jar -Dfile.encoding=utf-8 ${JAR_NAME} &
+#nohup java -javaXms128m -Xmx256m -jar -Dfile.encoding=utf-8 ${JAR_NAME} &
+nohup java -jar ${JAR_NAME} >consoleMsg.log 2>&1 &
