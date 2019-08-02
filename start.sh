4765f85eee036a23e38a82e53709bd4f8e4f4d2e
lyl <251409016@qq.com>
2019-08-02 14:56:29 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 export BUILD_ID=dontKillMe
-echo $BUILD_ID
-
 JAR_NAME='qxl-web.jar'
-cd ~/.jenkins/workspace/qxl-web/target || exit
+JAR_FILE=~/.jenkins/workspace/qxl-web/target/$JAR_NAME
+if [ ! -f "$JAR_FILE" ]; then
+  exit
+else
+  cp $JAR_FILE ~/qxl-web/$JAR_NAME
+fi
+
+cd ~/qxl-web/ || exit
 pwd
 
 echo "Startup SpringBoot Application"
 #nohup java -javaXms128m -Xmx256m -jar -Dfile.encoding=utf-8 ${JAR_NAME} &
-nohup java -jar ${JAR_NAME} > consoleMsg.log 2>&1 &
+nohup java -jar ${JAR_NAME} >consoleMsg.log 2>&1 &
 #pid='ps -ef | grep '${JAR_NAME}' |  grep -v grep | awk '{print $2}''
 #if [ -n "$pid" ]
 #then
