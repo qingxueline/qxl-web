cd2831db63c3d3683a8fb250fb5c3959ff5c0264
lyl <251409016@qq.com>
2019-08-02 11:57:38 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 JAR_NAME='qxl-web.jar'
 cd ~/.jenkins/workspace/qxl-web/target || exit
 pwd
+
+echo "Start SpringBoot Application"
 #nohup java -javaXms128m -Xmx256m -jar -Dfile.encoding=utf-8 ${JAR_NAME} &
 nohup java -jar qxl-web.jar > consoleMsg.log 2>&1 &
 #pid='ps -ef | grep '${JAR_NAME}' |  grep -v grep | awk '{print $2}''
