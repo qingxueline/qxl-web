17dc33059f2eee58b4e38b8212dbe5100a754d3d
lyl <251409016@qq.com>
2019-08-02 10:46:12 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -5,3 +5,4 @@ pwd
 #nohup java -javaXms128m -Xmx256m -jar -Dfile.encoding=utf-8 ${JAR_NAME} &
 nohup java -jar ${JAR_NAME}.jar>consoleMsg.log 2>&1 &
 #pid='ps -ef | grep '${JAR_NAME}'.jar | grep -v grep'
+cd ~
