7f1ee7f9f3b19638e19410250fbd9151a0285772
lyl <251409016@qq.com>
2019-08-02 11:10:19 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ cd ~/.jenkins/workspace/qxl-web/target
 pwd
 #nohup java -javaXms128m -Xmx256m -jar -Dfile.encoding=utf-8 ${JAR_NAME} &
 nohup nohup java -jar ${JAR_NAME} >consoleMsg.log 2>&1 &
-pid='ps -ef | grep '${JAR_NAME}' |  grep -v grep | awk '{print $2}''
+#pid='ps -ef | grep '${JAR_NAME}' |  grep -v grep | awk '{print $2}''
