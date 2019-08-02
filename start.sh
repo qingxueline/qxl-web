bde1cb209aa6fb68444e99c4e76a77d5476b32b2
lyl <251409016@qq.com>
2019-08-02 11:22:20 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ JAR_NAME='qxl-web.jar'
 cd ~/.jenkins/workspace/qxl-web/D:/JAR-Target/web-api
 pwd
 #nohup java -javaXms128m -Xmx256m -jar -Dfile.encoding=utf-8 ${JAR_NAME} &
-nohup nohup java -jar ${JAR_NAME} >consoleMsg.log 2>&1 &
+nohup java -jar ${JAR_NAME} >consoleMsg.log 2>&1 &
 #pid='ps -ef | grep '${JAR_NAME}' |  grep -v grep | awk '{print $2}''
