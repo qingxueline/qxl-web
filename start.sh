0ff04d6348609284ca8c1692c5d832cd29c26d80
lyl <251409016@qq.com>
2019-08-02 10:33:52 +0800

    去除docker插件

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ cd ~/.jenkins/workspace/qxl-web/target
 pwd
 #nohup java -javaXms128m -Xmx256m -jar -Dfile.encoding=utf-8 ${JAR_NAME} &
 nohup java -jar ${JAR_NAME}.jar>consoleMsg.log 2>&1 &
-pid='ps -ef | grep '${JAR_NAME}'.jar | grep -v grep | awk '{print $2}''
+pid='ps -ef | grep '${JAR_NAME}'.jar | grep -v grep'
 echo ${pid}
